e464ef994ff2e38ba94d0bfef0396776a482947f
OMAKAR4748 <omkardesai771@gmail.com>
2024-12-06 08:36:18 +0530

    servlet

---
+++ rain/src/main/java/com/xworkz/rain/servlet/SecondServlet.java
@@ -0,0 +1,38 @@
+package com.xworkz.rain.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+@WebServlet(urlPatterns = "/second", loadOnStartup = 1)
+public class SecondServlet extends HttpServlet {
+
+    public SecondServlet()
+    {
+
+        System.out.println("running no-args const of firstServlet");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Executing service in FirstServlet");
+        String url = req.getRequestURI().toString();
+        System.out.println("URL :" +url);
+        Enumeration<String> enumeration = req.getHeaderNames();
+        while (enumeration.hasMoreElements())
+        {
+            String headerName = enumeration.nextElement();
+            String value = req.getHeader(headerName);
+            System.out.println(headerName +" :"+value);
+        }
+
+        PrintWriter respWriter = resp.getWriter();
+        respWriter.println("Banana is working......");
+    }
+}
+
