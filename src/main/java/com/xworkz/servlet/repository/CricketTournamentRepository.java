146d9e99bf4fc0d4f47dc1b66fa350e95432bd84
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:08:39 +0530

    Update CricketTournamentRepository.java

--- src/main/java/com/xworkz/servlet/repository/CricketTournamentRepository.java
+++ src/main/java/com/xworkz/servlet/repository/CricketTournamentRepository.java
@@ -2,6 +2,14 @@ package com.xworkz.servlet.repository;
 
 import com.xworkz.servlet.dto.CricketTournamentDto;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface CricketTournamentRepository {
     boolean save(CricketTournamentDto cricketTournamentDto);
+
+    default List<CricketTournamentDto> getAllCricketForm()
+    {
+        return Collections.emptyList();
+    }
 }
