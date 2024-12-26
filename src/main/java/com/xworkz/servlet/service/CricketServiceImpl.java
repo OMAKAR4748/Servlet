200d9e0a1d61ccde36fcc9928792d6a04c401bf1
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:02:36 +0530

    Update CricketServiceImpl.java

--- src/main/java/com/xworkz/servlet/service/CricketServiceImpl.java
+++ src/main/java/com/xworkz/servlet/service/CricketServiceImpl.java
@@ -6,42 +6,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class CricketServiceImpl implements CricketService{
+
+    private CricketService cricketService = new CricketServiceImpl();
 
 
     @Override
     public boolean save(CricketTournamentDto cricketTournamentDto) {
-
-        Connection connection = null;
-        Statement statement = null;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("connection successful");
-            String url = "jdbc:mysql://localhost:3306/xworkz";
-            String username = "root";
-            String password = "root";
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println(" jdbc connected succesfully");
-
-            String insert = "insert into CricketTournament values(0,'"+cricketTournamentDto.getTeamName()+"','"+cricketTournamentDto.getCaptainName()+"',"+cricketTournamentDto.getContactNumber()+",'"+cricketTournamentDto.getEmail()+"',"+cricketTournamentDto.getNumPlayers()+",'"+cricketTournamentDto.getCoachName()+"','"+cricketTournamentDto.getHomeGround()+"','"+cricketTournamentDto.getSponsorsName()+"')";
-
-            statement = connection.createStatement();
-
-            int row = statement.executeUpdate(insert);
-            System.out.println("no of rows inserted :" + row);
-
-        } catch (ClassNotFoundException | SQLException c) {
-            System.out.println("jdbc class not found" + c.getMessage());
-
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                save(cricketTournamentDto);
-            }
-        }
+        System.out.println("saved");
+        cricketService.save(cricketTournamentDto);
         return true;
+    }
+
+    @Override
+    public List<CricketTournamentDto> getAllCricketForm()
+    {
+        return CricketService.super.getAllCricketForm();
     }
 }
