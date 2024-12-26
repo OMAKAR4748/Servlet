2bb5b77427487a0ba286ffa18dffb8f0f87c364d
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:05:55 +0530

    Update SweetServiceImpl.java

--- src/main/java/com/xworkz/servlet/service/SweetServiceImpl.java
+++ src/main/java/com/xworkz/servlet/service/SweetServiceImpl.java
@@ -3,49 +3,18 @@ package com.xworkz.servlet.service;
 import com.xworkz.servlet.dto.SweetFormDto;
 import com.xworkz.servlet.repository.SweetRepository;
 import com.xworkz.servlet.repository.SweetRepositoryImpl;
+import java.util.List;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class SweetServiceImpl implements SweetService{
+public class SweetServiceImpl implements SweetService {
     private SweetRepository sweetRepository = new SweetRepositoryImpl();
-
 
     @Override
     public boolean save(SweetFormDto sweetFormDto) {
-        System.out.println("saved");
-        Connection connection = null;
-        Statement statement = null;
+        return sweetRepository.save(sweetFormDto);
+    }
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("connection successful");
-            String url = "jdbc:mysql://localhost:3306/xworkz";
-            String username = "root";
-            String password = "root";
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println(" jdbc connected succesfully");
-
-            String insert = "insert into SweetForm values(0,'"+sweetFormDto.getShopName()+"','"+sweetFormDto.getSweetName()+"',"+sweetFormDto.getWeight()+","+sweetFormDto.getQuantity()+","+sweetFormDto.getPrice()+")";
-
-            statement = connection.createStatement();
-
-            int row = statement.executeUpdate(insert);
-            System.out.println("no of rows inserted :" + row);
-
-        } catch (ClassNotFoundException | SQLException c) {
-            System.out.println("jdbc class not found" + c.getMessage());
-
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                save(sweetFormDto);
-            }
-        }
-        boolean result =sweetRepository.save(sweetFormDto);
-        return result;
+    @Override
+    public List<SweetFormDto> getAllSweetForms() {
+        return sweetRepository.getAllSweetForms();
     }
 }
