efb552fb7e51f3aab159b051cbb1ddd0e74ba1d7
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:10:10 +0530

    Update MilkRepositoryImpl.java

--- src/main/java/com/xworkz/servlet/repository/MilkRepositoryImpl.java
+++ src/main/java/com/xworkz/servlet/repository/MilkRepositoryImpl.java
@@ -1,45 +1,46 @@
 package com.xworkz.servlet.repository;
 
 import com.xworkz.servlet.dto.MilkFormDto;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MilkRepositoryImpl implements MilkRepository{
+public class MilkRepositoryImpl implements MilkRepository {
+
     @Override
     public boolean save(MilkFormDto milkFormDto) {
-        System.out.println("MilkRepositoryImpl is saved");
-
-        Connection connection = null;
-        Statement statement = null;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("connection successful");
-            String url = "jdbc:mysql://localhost:3306/xworkz";
-            String username = "root";
-            String password = "root";
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println(" jdbc connected succesfully");
-            String insert = "insert into MilkForm values(0,'"+milkFormDto.getBrand()+ "','"+milkFormDto.getType()+"',"+milkFormDto.getQuantity()+","+milkFormDto.getTotalPrice()+")";
-
-            statement = connection.createStatement();
-
-            int row = statement.executeUpdate(insert);
-            System.out.println("no of rows inserted :" + row);
-
-        } catch (ClassNotFoundException | SQLException c) {
-            System.out.println("jdbc class not found" + c.getMessage());
-
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                save(milkFormDto);
-            }
-        }
         return true;
+    }
+
+    @Override
+    public List<MilkFormDto> getAllMilkForms() {
+        List<MilkFormDto> milkForms = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/xworkz", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM milkForm")) {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            while (resultSet.next()) {
+                String brand = resultSet.getString("brand");
+                String type = resultSet.getString("type");
+                int quantity = resultSet.getInt("quantity");
+                int totalPrice = resultSet.getInt("totalPrice");
+
+                MilkFormDto milkForm = new MilkFormDto(brand, type, quantity, totalPrice);
+                milkForms.add(milkForm);
+
+                System.out.println("No of order :"+statement.getFetchSize());
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return milkForms;
     }
 }
