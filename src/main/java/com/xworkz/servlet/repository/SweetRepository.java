ff4addc05078f679abbc9dcc8d160ea668af3a92
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:11:32 +0530

    Update SweetRepository.java

--- src/main/java/com/xworkz/servlet/repository/SweetRepository.java
+++ src/main/java/com/xworkz/servlet/repository/SweetRepository.java
@@ -2,6 +2,14 @@ package com.xworkz.servlet.repository;
 
 import com.xworkz.servlet.dto.SweetFormDto;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface SweetRepository {
     boolean save(SweetFormDto sweetFormDto);
+
+    default List<SweetFormDto> getAllSweetForms()
+    {
+        return Collections.emptyList();
+    }
 }
