1ab28dd1abc8ef631de1f42bd22064ea12071305
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:09:37 +0530

    Update MilkRepository.java

--- src/main/java/com/xworkz/servlet/repository/MilkRepository.java
+++ src/main/java/com/xworkz/servlet/repository/MilkRepository.java
@@ -2,6 +2,13 @@ package com.xworkz.servlet.repository;
 
 import com.xworkz.servlet.dto.MilkFormDto;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface MilkRepository {
     boolean save(MilkFormDto milkFormDto);
+
+    default List<MilkFormDto> getAllMilkForms(){
+       return Collections.emptyList();
+    }
 }
