48a366e3d0e7c41054cb6153857a9ded4451724c
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:12:12 +0530

    Update SweetRepositoryImpl.java

--- src/main/java/com/xworkz/servlet/repository/SweetRepositoryImpl.java
+++ src/main/java/com/xworkz/servlet/repository/SweetRepositoryImpl.java
@@ -1,45 +1,45 @@
 package com.xworkz.servlet.repository;
 
 import com.xworkz.servlet.dto.SweetFormDto;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SweetRepositoryImpl implements SweetRepository{
+public class SweetRepositoryImpl implements SweetRepository {
+
     @Override
     public boolean save(SweetFormDto sweetFormDto) {
-
-        Connection connection = null;
-        Statement statement = null;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("connection successful");
-            String url = "jdbc:mysql://localhost:3306/xworkz";
-            String username = "root";
-            String password = "root";
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println(" jdbc connected succesfully");
-
-            String insert = "insert into SweetForm values(0,'"+sweetFormDto.getShopName()+"','"+sweetFormDto.getSweetName()+"',"+sweetFormDto.getWeight()+","+sweetFormDto.getQuantity()+","+sweetFormDto.getPrice()+")";
-
-            statement = connection.createStatement();
-
-            int row = statement.executeUpdate(insert);
-            System.out.println("no of rows inserted :" + row);
-
-        } catch (ClassNotFoundException | SQLException c) {
-            System.out.println("jdbc class not found" + c.getMessage());
-
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                save(sweetFormDto);
-            }
-        }
         return true;
+    }
+
+    @Override
+    public List<SweetFormDto> getAllSweetForms() {
+        List<SweetFormDto> sweetForms = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/xworkz", "root", "root");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM Sweets")) {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            while (resultSet.next()) {
+                String shopName = resultSet.getString("shopName");
+                String sweetName = resultSet.getString("sweetName");
+                double weight = resultSet.getDouble("weight");
+                int quantity = resultSet.getInt("quantity");
+                double price = resultSet.getDouble("price");
+
+                SweetFormDto sweetForm = new SweetFormDto(shopName, sweetName, weight, quantity, price);
+                sweetForms.add(sweetForm);
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return sweetForms;
     }
 }
