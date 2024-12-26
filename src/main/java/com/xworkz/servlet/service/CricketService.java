db9ab164c66761d49a25e781ddfd67cd98ca6a2f
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:01:40 +0530

    Update CricketService.java

--- src/main/java/com/xworkz/servlet/service/CricketService.java
+++ src/main/java/com/xworkz/servlet/service/CricketService.java
@@ -1,7 +1,18 @@
 package com.xworkz.servlet.service;
 
 import com.xworkz.servlet.dto.CricketTournamentDto;
+import com.xworkz.servlet.dto.MilkFormDto;
+
+import java.util.List;
 
 public interface CricketService {
-    public boolean save(CricketTournamentDto dto);
+
+    boolean save(CricketTournamentDto cricketTournamentDto);
+
+    default List<CricketTournamentDto> getAllCricketForm()
+    {
+        return getAllCricketForm();
+    }
+
+
 }
