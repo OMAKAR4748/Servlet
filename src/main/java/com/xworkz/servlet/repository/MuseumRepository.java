75fd986320a974a2a9f74030053f882d3685c21e
Omkar Desai <150238469+OMAKAR4748@users.noreply.github.com>
2024-12-26 23:10:36 +0530

    Update MuseumRepository.java

--- src/main/java/com/xworkz/servlet/repository/MuseumRepository.java
+++ src/main/java/com/xworkz/servlet/repository/MuseumRepository.java
@@ -2,6 +2,14 @@ package com.xworkz.servlet.repository;
 
 import com.xworkz.servlet.dto.MuseumTicketFormDto;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface MuseumRepository {
     boolean save(MuseumTicketFormDto museumTicketFormDto);
+
+    default List<MuseumTicketFormDto> getAllMusemTicketForm()
+    {
+        return Collections.emptyList();
+    }
 }
